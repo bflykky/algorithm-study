216b75f41b7f2ed8e3cf1865da579942e670a44a
bflykky <kkyxw@soongsil.ac.kr>
2024-09-19 18:16:32 +0900

    feat: 백준 2749번 문제 - 피보나치 수 3 재풀이 및 풀이 방식 작성

--- src/main/java/baekjoon/Problem2749.java
+++ src/main/java/baekjoon/Problem2749.java
@@ -6,6 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/*
+ * 피사노 주기를 이용하여 풀이해야 한다.
+ * n번째 피보나치 수를 1_000_000으로 나눈 나머지 K를 구하기 위해 n번 반복하는 게 아니라,
+ * 피사노 주기 M의 주기에 대응해 K의 집합이 반복된다는 사실을 이용함으로써 n번 반복이 아닌 M번만 반복하여 시간을 단축해야 한다.
+ * 피사노 주기를 찾는 공식은 있지만, 만약 기억이 안난다면 0번째, 1번째 피보나치 수를 1_000_000번 나눈 나머지는 0, 1이란 걸 이용해
+ * 연속으로 0, 1이 나오는 경우를 찾아 주기를 예측할 수 있다.
+ */
 public class Problem2749 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,18 +23,16 @@ public class Problem2749 {
 
         int fibonacciCount = Math.toIntExact(Long.parseLong(br.readLine()) % PISANO_PERIOD);
         if (fibonacciCount == 0 || fibonacciCount == 1) {
-            bw.write(fibonacciCount + "\n");
+            bw.write(fibonacciCount % MILLION + "\n");
         } else {
-            long formerAndFormerFibonacci = 0L;
-            long formerFibonacci = 1L;
-            long result = 0L;
+            long[] fibonacci = new long[fibonacciCount + 1];
+            fibonacci[0] = 0 % MILLION;
+            fibonacci[1] = 1 % MILLION;
             for (int i = 2; i < fibonacciCount + 1; i++) {
-                result = (formerFibonacci % MILLION) + (formerAndFormerFibonacci % MILLION) % MILLION;
-                formerAndFormerFibonacci = formerFibonacci % MILLION;
-                formerFibonacci = result;
+                fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % MILLION;
             }
 
-            bw.write(result % MILLION + "\n");
+            bw.write(fibonacci[fibonacciCount] + "\n");
         }
 
         bw.flush();
