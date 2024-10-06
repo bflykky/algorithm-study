a85130cba17296d2620f0617eba616c11d3eb872
bflykky <kkyxw@soongsil.ac.kr>
2024-10-06 22:31:24 +0900

    refactor: 랜덤 문제 추출기 코드 리팩토링

--- src/main/java/leetcode/Top150InterviewsDraw.java
+++ src/main/java/leetcode/Top150InterviewsDraw.java
@@ -3,22 +3,28 @@ package main.java.leetcode;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 public class Top150InterviewsDraw {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("./problem_list.txt"));
-
-        Set<String> problemList = new HashSet<>();
+        String str = "abc";
+        str.indexOf("str");
+        List<String> problemList = new ArrayList<>();
         String problem = br.readLine();
         while (problem != null) {
             problemList.add(problem);
             problem = br.readLine();
         }
+        long seed = System.currentTimeMillis();
+        Collections.shuffle(problemList, new Random(seed));
 
-        System.out.println(problemList.size());
-        System.out.printf("오늘 풀 문제: %s\n", problemList.stream().findFirst().get());
+        System.out.printf("오늘 풀 문제: %s\n", problemList.get(0));
 
     }
 }
